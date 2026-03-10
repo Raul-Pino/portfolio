199a381c2ea791f72bd97c72c85bcef035e1e98e
raul <loaizapinoraul@gmail.com>
2026-03-10 11:43:08 +0100

    configuración de angular y creación de header y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { About } from './components/about/about';
+import { Skills } from './components/skills/skills';
+import { Projects } from './components/projects/projects';
+import { Contact } from './components/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: About
+    },
+    {
+        path: "skills",
+        component: Skills
+    },
+    {
+        path: "projects",
+        component: Projects
+    },
+    {
+        path: "contact",
+        component: Contact
+    },
+];
