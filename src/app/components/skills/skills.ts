fd43164debfa5a0854d2051c3d7f1e0b6ce257d3
raul <loaizapinoraul@gmail.com>
2026-03-11 12:40:03 +0100

    Implementación de la página de mis habilidades y correcciones del header

--- src/app/components/skills/skills.ts
+++ src/app/components/skills/skills.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { DataService } from '../../services/data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.css',
 })
-export class Skills {
+export class Skills implements OnInit{
+  misHabilidades: any[] = [];
 
+  constructor(private dataService: DataService, private cd: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.dataService.getDatos().subscribe(data => {
+      this.misHabilidades = data.habilidades;
+      this.cd.detectChanges();
+    })
+  }
 }
---
+++ src/app/services/data.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http: HttpClient) { }
+
+  getDatos(): Observable<any> {
+    return this.http.get('/data.json');
+  }
+}
