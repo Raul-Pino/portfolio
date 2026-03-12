56d1eff2866be26f962fcdbe9b06a760e9478ca3
raul <loaizapinoraul@gmail.com>
2026-03-12 08:41:48 +0100

    Implementación de las páginas sobre mi y contacto

--- src/app/components/contact/contact.ts
+++ src/app/components/contact/contact.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
 export class Contact {
+  enviado = false;
 
+    enviarFormulario(formulario: any) {
+      console.log('Formulario enviado:', formulario.value);
+      
+      this.enviado = true;
+
+      setTimeout(() => {
+        formulario.reset();
+        this.enviado = false;
+      }, 3000);
+    }
 }
